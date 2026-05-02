f3c31011c1c1874a627552c033412e7a76c4f1c8
fideskey <fideskey@outlook.com>
2026-05-02 17:04:56 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      // AtacadoDaFesta + ExtraFesta (Tray Commerce)
+      { protocol: "https", hostname: "images.tcdn.com.br" },
+      { protocol: "https", hostname: "*.tcdn.com.br" },
+      // Matsumoto (VTEX)
+      { protocol: "https", hostname: "*.vtexassets.com" },
+      { protocol: "https", hostname: "*.vteximg.com.br" },
+      { protocol: "https", hostname: "lojasmatsumoto.vtexassets.com" },
+      // Elo7
+      { protocol: "https", hostname: "*.elo7.com.br" },
+      { protocol: "https", hostname: "img.elo7.com.br" },
+      { protocol: "https", hostname: "images.elo7.com.br" },
+      // Cromus / SemaanPari
+      { protocol: "https", hostname: "*.cromus.com.br" },
+      { protocol: "https", hostname: "*.semaanpari.com.br" },
+      // Genérico para cualquier otro proveedor
+      { protocol: "https", hostname: "**" },
+    ],
+  },
+};
 
 module.exports = nextConfig;
