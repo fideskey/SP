515e97e7581a90c960ea54bb7dc69ebe42cdc679
fideskey <fideskey@outlook.com>
2026-05-03 03:07:02 -0300

    Update menuData.ts

--- src/components/Header/menuData.ts
+++ src/components/Header/menuData.ts
@@ -1,19 +1,23 @@
-import { Menu } from "@/types/Menu";
+import { Menu } from "@/types/menu";
 
 const menuData: Menu[] = [
-  { id: 1, title: "Inicio",    path: "/",                    newTab: false },
-  { id: 2, title: "Tienda",    path: "/shop-with-sidebar",  newTab: false },
-  { id: 3, title: "Contacto",  path: "/contact",             newTab: false },
   {
-    id: 4,
-    title: "Páginas",
+    id: 1,
+    title: "Inicio",
+    path: "/",
     newTab: false,
-    submenu: [
-      { id: 41, title: "Mi cuenta",        path: "/my-account",  newTab: false },
-      { id: 42, title: "Carrito",          path: "/cart",        newTab: false },
-      { id: 43, title: "Lista de deseos",  path: "/wishlist",    newTab: false },
-      { id: 44, title: "Checkout",         path: "/checkout",    newTab: false },
-    ],
+  },
+  {
+    id: 2,
+    title: "Tienda",
+    path: "/shop",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Contacto",
+    path: "/contact",
+    newTab: false,
   },
 ];
 
