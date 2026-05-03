74ce998c23cd1f432bfa661a8f6f9376ad0d4666
fideskey <fideskey@outlook.com>
2026-05-02 22:14:02 -0300

    Add files via upload

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,24 +1,17 @@
-import React from "react";
 import Hero from "./Hero";
 import Categories from "./Categories";
-import NewArrival from "./NewArrivals";
-import PromoBanner from "./PromoBanner";
+import NewArrivals from "./NewArrivals";
 import BestSeller from "./BestSeller";
-import CounDown from "./Countdown";
-import Testimonials from "./Testimonials";
-import Newsletter from "../Common/Newsletter";
+import Countdown from "./Countdown";
 
 const Home = () => {
   return (
     <main>
       <Hero />
       <Categories />
-      <NewArrival />
-      <PromoBanner />
+      <NewArrivals />
       <BestSeller />
-      <CounDown />
-      <Testimonials />
-      <Newsletter />
+      <Countdown />
     </main>
   );
 };
