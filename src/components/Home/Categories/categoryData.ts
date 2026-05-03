bffbc7403e13cac961b7189398e942f8145809f3
fideskey <fideskey@outlook.com>
2026-05-02 22:20:14 -0300

    Add files via upload

--- src/components/Home/Categories/categoryData.ts
+++ src/components/Home/Categories/categoryData.ts
@@ -1,44 +1,11 @@
 const data = [
-  {
-    title: "Globos",
-    id: 1,
-    img: "/images/categories/categories-01.png",
-  },
-  {
-    title: "Decoración",
-    id: 2,
-    img: "/images/categories/categories-02.png",
-  },
-  {
-    title: "Fiestas Temáticas",
-    id: 3,
-    img: "/images/categories/categories-03.png",
-  },
-  {
-    title: "Recuerdos",
-    id: 4,
-    img: "/images/categories/categories-04.png",
-  },
-  {
-    title: "Carnaval y Disfraces",
-    id: 5,
-    img: "/images/categories/categories-05.png",
-  },
-  {
-    title: "Cotillón",
-    id: 6,
-    img: "/images/categories/categories-06.png",
-  },
-  {
-    title: "Vajilla Descartable",
-    id: 7,
-    img: "/images/categories/categories-07.png",
-  },
-  {
-    title: "Velas",
-    id: 8,
-    img: "/images/categories/categories-04.png",
-  },
+  { title: "Globos",               id: 1, img: "/images/categories/categories-01.png" },
+  { title: "Decoración",           id: 2, img: "/images/categories/categories-02.png" },
+  { title: "Fiestas Temáticas",    id: 3, img: "/images/categories/categories-03.png" },
+  { title: "Recuerdos",            id: 4, img: "/images/categories/categories-04.png" },
+  { title: "Carnaval y Disfraces", id: 5, img: "/images/categories/categories-05.png" },
+  { title: "Cotillón",             id: 6, img: "/images/categories/categories-06.png" },
+  { title: "Vajilla Descartable",  id: 7, img: "/images/categories/categories-07.png" },
+  { title: "Velas",                id: 8, img: "/images/categories/categories-04.png" },
 ];
-
 export default data;
